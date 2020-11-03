2a9fe1443a8adee8b4057848db54cb02526bcb57
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-11-03 17:01:03 -0500

    Add logic to check for Python on system

--- environment.sh
+++ environment.sh
@@ -14,12 +14,17 @@ then
     pip list --outdated
 
     # upgrade pip
-    # pip install --upgrade
+    pip install --upgrade pip
 
     echo "pip updated"
 else echo "pip not found."
 fi;
 
+
+if python -V | python3 -V
+    then echo "Python available"
+    else echo "Python not available.  Install if needed"
+fi;
 
 mkdir Results; cd Results
 
