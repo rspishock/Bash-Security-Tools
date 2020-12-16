f2f41ab50ccc25f7c6b27aa060bd995a422a6d15
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-12-15 20:31:18 -0500

    Code refactor

--- nmap-insert.py
+++ nmap-insert.py
@@ -18,29 +18,48 @@ def main():
         usage()
         exit(0)
 
-        infile = open(sys.argv[1], 'r')
+    infile = open(sys.argv[1], 'r')
 
-        for event, elem in iterparse(infile):
-            if elem.tag == 'host':
-                # add defaults to cover missing values
-                macaddr = {}
-                hostnames = []
-                os = []
-                addrs = elem.findall('address')
-                # all addresses, IPv4, IPv6 (if exists), MAC
+    for event, elem in iterparse(infile):
+        if elem.tag == 'host':
+            # add defaults to cover missing values
+            macaddr = {}
+            hostnames = []
+            os = []
+            addrs = elem.findall('address')
+            # all addresses, IPv4, IPv6 (if exists), MAC
+            for addr in addrs:
+                type = add.get('addrtype')
+                if (type == 'ipv4'):
+                    ipaddr = addr.get('addr')
+                if (type == 'mac'):  # there are two useful things to get here
+                    macaddr = {'addr': addr.get('addr'),
+                               'vendor': addr.get('vendor')}
 
-                for addr in addrs:
-                    type = add.get('addrtype')
-                    if (type == 'ipv4'):
-                        ipaddr = addr.get('addr')
-                    if (type == 'mac'):  # there are two useful things to get here
-                        macaddr = {'addr': addr.get('addr'),
-                                   'vendor': addr.get('vendor')}
+            hostlist = elemfindall('hostname')
+            for host in hostlist:
+                hostnames += [{'name': host.get('name'),
+                               'type': hsot.get('type')}]
 
-                        hostlist = elemfindall('hostname')
-                        for host in hostlist:
-                            hostnames += [{'name': host.get('name'),
-                                           'type': hsot.get('type')}]
+            # OS detection
+            # we will be conservative and put it all here
+            oslist = elem.find('os').findall('osmatch')
+            for oseach in oslist:
+                cpelist = []
+                for cpe in cpelist.findall('osclass'):
+                    cpelist += {cpe.findtext('cpe')}
 
-                        # OS detection
-                        
+                    os += [{'osname': osearch.get('name'), 
+                            'accuracy': osearch.get('accuracy'), 
+                            'cpe': cpelist}]
+
+            portlist = elem.find('ports').findall('port')
+            ports = []
+            for port in portlist:
+                ports += [{'proto': port.get('protocol'), 
+                           'port': port.get('portid'), 
+                            'state': port.find('state').get('state')
+                            'service': port.find('service').get('name')}]
+            elem.clear()
+
+                                    
