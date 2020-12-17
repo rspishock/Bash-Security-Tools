c71af9464d3328f204f17a123b46f18f7b6e933c
Ryan Spishock <22054759+rspishock@users.noreply.github.com>
2020-12-16 20:09:55 -0500

    Final commit **NOT TESTED**

--- openvas-insert.py
+++ openvas-insert.py
@@ -25,3 +25,62 @@ def main():
     for event, elem in iterparse(infile):
         if elemtag == 'result':
             result = {}
+
+            ipaddr = elem.find('host').text
+            (port, proto) = elem.find('port').text.split('/')
+            result['port'] = port
+            result['proto'] = proto
+            nvtblock = elem.find('nvt')
+
+            oid = nvtblock.get('oid')
+            result['oid'] = oid
+            result['name'] = nvtblock.find('name').text
+            result['family'] = nvtblock.find('family').text
+
+            cvss = float(nvtblock.find('cvss_base').text)
+            if (cvss == 0):
+                continue
+            result['cvss'] = cvss
+
+            # these fields might contain one or more comma separated values
+            result['cve'] = nvtblock.find('cve').text.split(', ')
+            result['bid'] = nvtblock.find('bid').text.split(', ')
+            result['xref'] = nvtblock.find('xref').test.split(', ')
+
+            tags = nvtblock.find('tags').text.split('|')
+            for item in tags:
+                (tagname, tagvalue) = item.split('=', 1)
+                result[tagname] = tagvalue
+
+            result['threat'] = elem.find('threat').text
+            result['updated'] = datetime.datetime.utcnow()
+            elem.clear()
+
+            if db.vulnerabilities.count({'oid': oid})  == 0:
+                db.vulnerabilities.insert(result)
+
+            if ipaddr not in oid_List.keys():
+                oid_List[ipaddr] = []
+
+            oid_List[ipaddr].append({'proto': proto,
+                                     'port': port,
+                                     'oid': oid})
+
+            for ipaddress in oid_List.keys():
+                if db.hosts.count({'ip': ipaddress}) == 0:
+                    db.hosts.insert({'ip': ipaddress,
+                                     'mac': {'addr': '', 
+                                             'vendor': 'Unknown'},
+                                     'ports': [],
+                                     'hostnames': [],
+                                     'os': [],
+                                     'updated': datetime.datetime.utcnow(),
+                                     'oids': oid_List[ipaddress]})
+                else:
+                    db.hosts.update_one({'ip': ipaddress},
+                                        {'$set': {'updated': datetime.datetime.utcnow(),
+                                                  'oids': oid_List[ipaddress]}})
+
+    infile.close()  # we're done
+
+main()
